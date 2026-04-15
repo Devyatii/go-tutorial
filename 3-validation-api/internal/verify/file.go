929babfcec763affb69c125cf51f82329d6b5d1c
kirillov <kirillov@protei-lab.ru>
2026-04-15 19:16:18 +0300

    3-validation-api - added verify method

---
+++ 3-validation-api/internal/verify/file.go
@@ -0,0 +1,79 @@
+package verify
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const EMAIL_DATA = "emailData.json"
+
+type EmailData struct {
+	Email string
+	Hash  string
+}
+
+func SaveEmailData(emailData EmailData) error {
+
+	emailDataList, err := getEmailData()
+
+	if err != nil {
+		return err
+	}
+
+	for index, emailDataItem := range emailDataList {
+		if emailDataItem.Email == emailData.Email {
+			emailDataList[index].Hash = emailData.Hash
+			return nil
+		}
+	}
+
+	emailDataList = append(emailDataList, emailData)
+
+	return writeEmailData(emailDataList)
+}
+
+func ReadEmailData(hash string) (bool, error) {
+	emailDataList, err := getEmailData()
+	isFound := false
+
+	if err != nil {
+		return isFound, err
+	}
+
+	var updatedEmailDataList []EmailData
+
+	for _, emailDataItem := range emailDataList {
+		if emailDataItem.Hash == hash {
+			isFound = true
+			continue
+		}
+		updatedEmailDataList = append(updatedEmailDataList, emailDataItem)
+	}
+
+	return isFound, writeEmailData(emailDataList)
+}
+
+func getEmailData() ([]EmailData, error) {
+	content, err := os.ReadFile(EMAIL_DATA)
+	if err != nil {
+		return nil, err
+	}
+
+	var emailDataList []EmailData
+
+	unmarshalErr := json.Unmarshal(content, &emailDataList)
+	if unmarshalErr != nil {
+		return nil, unmarshalErr
+	}
+
+	return emailDataList, nil
+}
+
+func writeEmailData(emailDataList []EmailData) error {
+	updatedContent, err := json.MarshalIndent(emailDataList, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(EMAIL_DATA, updatedContent, 0644)
+}
--- 3-validation-api/internal/verify/handler.go
+++ 3-validation-api/internal/verify/handler.go
@@ -3,11 +3,13 @@ package verify
 import (
 	"3-validation-api/configs"
 	"3-validation-api/pkg/response"
+	"3-validation-api/pkg/utils"
 	"encoding/json"
 	"fmt"
-	"github.com/jordan-wright/email"
 	"net/http"
 	"net/smtp"
+
+	"github.com/jordan-wright/email"
 )
 
 type VerifyHandlerDeps struct {
@@ -41,14 +43,15 @@ func (handler *VerifyHandler) Send() http.HandlerFunc {
 			response.Json(writer, err.Error(), 403)
 			return
 		}
-		e := email.NewEmail()
-		e.From = "Jordan Wright <test@gmail.com>"
-		e.To = []string{"test@example.com"}
-		e.Bcc = []string{"test_bcc@example.com"}
-		e.Cc = []string{"test_cc@example.com"}
-		e.Subject = "Awesome Subject"
-		e.Text = []byte("Text Body is, of course, supported!")
-		e.HTML = []byte("<h1>Fancy HTML is supported, too!</h1>")
+		link, hash := generateHashLink(emailAddr)
+		saveEmailDataErr := SaveEmailData(EmailData{Email: emailAddr, Hash: hash})
+		if saveEmailDataErr != nil {
+			response.Json(writer, err.Error(), 500)
+			return
+		}
+
+		e := makeEmailTemplate()
+		e.Text = []byte(link)
 		mailSendError := e.Send(fmt.Sprintf("%s:587", address), smtp.PlainAuth("", emailAddr, password, address))
 		if mailSendError != nil {
 			response.Json(writer, mailSendError.Error(), 403)
@@ -60,5 +63,33 @@ func (handler *VerifyHandler) Send() http.HandlerFunc {
 
 func (handler *VerifyHandler) Verify() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		hash := request.PathValue("hash")
+		isFound, err := ReadEmailData(hash)
+		if err != nil {
+			response.Json(writer, err.Error(), 500)
+			return
+		}
+		if !isFound {
+			response.Json(writer, err.Error(), 403)
+			return
+		}
+		writer.WriteHeader(http.StatusOK)
 	}
+}
+
+func makeEmailTemplate() *email.Email {
+	e := email.NewEmail()
+	e.From = "Jordan Wright <test@gmail.com>"
+	e.To = []string{"test@example.com"}
+	e.Bcc = []string{"test_bcc@example.com"}
+	e.Cc = []string{"test_cc@example.com"}
+	e.Subject = "Verify email"
+	e.Text = []byte("Text Body is, of course, supported!")
+	e.HTML = []byte("<h1>Fancy HTML is supported, too!</h1>")
+	return e
+}
+
+func generateHashLink(email string) (link string, hash string) {
+	hash = utils.RandStringRunes(len(email))
+	return fmt.Sprintf("http://localhost:8081/verify/%s", hash), hash
 }
